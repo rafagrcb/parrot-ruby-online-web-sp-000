f52a5cebaf0695458f2ad67dbf9a10db07c76b5a
Rafael Garcia <rafagrcb@gmail.com>
2020-05-05 22:44:48 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase)
+def parrot
   puts phrase = "Pretty bird!"
  if phrase = "Pretty bird!"
    return phrase
